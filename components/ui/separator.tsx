6bd991c1edfa9f6cdff10cd81e27c1444fc282cb
ghyorka <ghyorkagospel@gmail.com>
2026-03-09 15:11:44 +0100

    feat(app-ui): harden dashboard states, upgrade rider/advertiser product views, and align shared UI primitives

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,18 +1,28 @@
-import * as React from "react";
-import * as SeparatorPrimitive from "@radix-ui/react-separator";
-import { cn } from "@/lib/utils";
+"use client"
 
-export function Separator({ className, orientation = "horizontal", decorative = true, ...props }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+import * as React from "react"
+import { Separator as SeparatorPrimitive } from "radix-ui"
+
+import { cn } from "@/lib/utils"
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
     <SeparatorPrimitive.Root
+      data-slot="separator"
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-border shrink-0",
-        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
-        className,
+        "shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
+        className
       )}
       {...props}
     />
-  );
+  )
 }
+
+export { Separator }
